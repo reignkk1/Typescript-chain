89314cae3fbf5530585705fe657330fa567faf56
reignkk1 <ruaals456@naver.com>
2022-11-07 15:15:03 +0900

    blockchain

--- src/index.ts
+++ src/index.ts
@@ -16,5 +16,40 @@ class Block implements BlockShape {
   ) {
     this.hash = Block.calculateHash(prevHash, height, data);
   }
-  static calculateHash(prevHash: string, height: number, data: string) {}
+  static calculateHash(prevHash: string, height: number, data: string) {
+    const toHash = `${prevHash}${height}${data}`;
+    return crypto.createHash("sha256").update(toHash).digest("hex");
+  }
 }
+
+class BlockChain {
+  private Blocks: Block[];
+  constructor() {
+    this.Blocks = [];
+  }
+  private getPrevHash() {
+    if (this.Blocks.length === 0) {
+      return "";
+    }
+    return this.Blocks[this.Blocks.length - 1].hash;
+  }
+  public addBlock(data: string) {
+    const newBlock = new Block(
+      this.getPrevHash(),
+      this.Blocks.length + 1,
+      data
+    );
+    this.Blocks.push(newBlock);
+  }
+  public getBlocks() {
+    return this.Blocks;
+  }
+}
+
+const blockChain = new BlockChain();
+
+blockChain.addBlock("hi");
+blockChain.addBlock("hello");
+blockChain.addBlock("bye");
+
+console.log(blockChain.getBlocks());
